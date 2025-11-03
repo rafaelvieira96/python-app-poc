fd4ac5dd1fb3869a13029f76184549686769e636
Rafael Martins <rafaelvieira96@hotmail.com>
2025-11-03 20:31:22 -0300

    running argocd

---
+++ charts/argocd/argo_start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rs=0
+
+kubectl scale statefulset -n argocd argocd-application-controller --replicas=$rs
+
+kubectl scale deployment argocd-applicationset-controller argocd-dex-server argocd-notifications-controller \
+  argocd-redis argocd-repo-server argocd-server --replicas=$rs -n argocd
+
+
