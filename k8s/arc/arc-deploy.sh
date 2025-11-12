d3bfa6081348e861dd9ebe665f48aacbd54210c0
Rafael Martins <rafaelvieira96@hotmail.com>
2025-11-12 19:56:20 -0300

    test agents

--- k8s/arc/arc-deploy.sh
+++ k8s/arc/arc-deploy.sh
@@ -1,13 +1,24 @@
-INSTALLATION_NAME="arc-runner-set"   # este nome será usado no runs-on:
-NAMESPACE="arc-runners"
-GITHUB_CONFIG_URL="https://github.com/rafaelvieira96/python-app-poc"
+NAMESPACE_SYS="arc-systems"
 
-# Se usar PAT (quickstart)
-GITHUB_PAT="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"
+helm install arc \
+  --namespace "${NAMESPACE_SYS}" \
+  --create-namespace \
+  oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+
+echo "aguarde...."
+
+sleep 5
+
+INSTALLATION_NAME="arc-runner-set"   # este nome será usado no runs-on:
+NAMESPACE_RUNNER="arc-runners"
+GITHUB_CONFIG_URL="https://github.com/rafaelvieira96/python-app-poc"
+GITHUB_PAT="token"
 
 helm install "${INSTALLATION_NAME}" \
-  --namespace "${NAMESPACE}" \
+  --namespace "${NAMESPACE_RUNNER}" \
   --create-namespace \
   --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
   --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+  --set minRunners=1 \
+  --set maxRunners=2 \
   oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
